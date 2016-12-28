95aab4bd53c05aa94f9ba40390830655a3ada6d0
Chris Munroe <idkm23@gmail.com>
2016-12-28 17:08:37 -0500

    Working Hello World, doesn't crash if ROS master is incorrect

--- app/src/main/java/edu/uml/cs/balloons/MainActivity.java
+++ app/src/main/java/edu/uml/cs/balloons/MainActivity.java
@@ -3,11 +3,25 @@ package edu.uml.cs.balloons;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.ros.android.RosActivity;
+import org.ros.node.NodeMainExecutor;
+
+import java.net.URI;
+
+public class MainActivity extends RosActivity {
+
+    protected MainActivity() {
+        super("SomeNode", "SomeNode", URI.create("http://129.63.16.221:11311"));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void init(NodeMainExecutor nodeMainExecutor) {
+
     }
 }
